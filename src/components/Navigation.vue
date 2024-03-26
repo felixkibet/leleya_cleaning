d29d0da8730a7fc3d9c28aa1ff4eb91d5ec84637
Adams Korir <adekorir@gmail.com>
2024-03-26 20:44:38 +0300

    improved routing

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,29 +1,39 @@
 <template>
-  <nav class="border navbar navbar-expand-sm bg-light navbar-light fixed-top" style="background-color: #2c365e">
+  <nav
+    class="border navbar navbar-expand-sm bg-light navbar-light fixed-top"
+    style="background-color: #2c365e"
+  >
     <div class="container">
       <a class="navbar-brand" href="#">Leleya Cleaning</a>
 
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
 
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="mb-2 navbar-nav ms-auto mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/">Home</a>
+            <RouterLink class="nav-link active" to="/">Home</RouterLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/about">About</a>
+            <RouterLink class="nav-link" to="/about">About</RouterLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/services">Services</a>
+            <RouterLink class="nav-link" to="/services">Services</RouterLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/gallery">Gallery</a>
+            <RouterLink class="nav-link" to="/gallery">Gallery</RouterLink>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/contact">Contact</a>
+            <RouterLink class="nav-link" to="/contact">Contact</RouterLink>
           </li>
         </ul>
       </div>
